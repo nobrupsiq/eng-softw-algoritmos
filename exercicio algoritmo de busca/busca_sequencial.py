a95b75d223928a2e01c104e02ea925dce69c700b
bruno <bpires.siqueira@gmail.com>
2025-03-19 17:50:14 -0300

    Etapa 1/2 concluida

---
+++ exercicio algoritmo de busca/busca_sequencial.py
@@ -0,0 +1,14 @@
+def busca_sequencial(lista, alvo):
+  for (index, numero) in enumerate(lista):
+    if numero == alvo:
+      return index
+  return -1
+    
+lista_de_numeros = [34,213,12,45,36,98,23,3,1,9,34]
+alvo = 130
+posicao_do_alvo = busca_sequencial(lista_de_numeros, alvo)
+
+if posicao_do_alvo != -1:
+  print(f'Posição do alvo {alvo} na lista: {posicao_do_alvo}')
+else:
+  print(f'O alvo {alvo} não se encontra na lista')

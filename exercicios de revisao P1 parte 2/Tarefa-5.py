a7873ef847807d9859a6f4da206f0c4d5a24f511
bruno <bpires.siqueira@gmail.com>
2025-04-14 10:44:03 -0300

    Revisao Tarefa-5 concluida

--- exercicios de revisao P1 parte 2/Tarefa-5.py
+++ exercicios de revisao P1 parte 2/Tarefa-5.py
@@ -4,4 +4,17 @@
 # • Acima de R$ 500 → 15%
 # O sistema deve receber o valor da compra e calcular o valor final com desconto aplicado
 
+def calcular_desconto(valor):
+  if valor > 0 and valor <= 100:
+    desconto = valor * 0.05
+  elif valor <= 500:
+    desconto = valor * 0.10
+  else:
+    desconto = valor * 0.15
 
+  return valor - desconto
+
+valor_do_produto = float(input('Valor do produto: '))
+resultado = calcular_desconto(valor_do_produto)
+
+print(f'Valor do produto com o desconto aplicado R${resultado}')

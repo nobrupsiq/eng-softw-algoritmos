33b72271318d51691f35382241dc2368b4085686
bruno <bpires.siqueira@gmail.com>
2025-05-27 10:50:43 -0300

    Exercicio 4 minha solução

---
+++ try-except/4.py
@@ -0,0 +1,33 @@
+# Exceções personalizadas: Escreva uma função que verifica se uma senha
+# possui no mínimo 8 caracteres e pelo menos um número. Se a senha não
+# atender aos requisitos, levante uma exceção com uma mensagem
+# personalizada. Trate a exceção e mostre a mensagem ao usuário.
+
+
+# O CÓDIGO E A SOLUÇÃO QUE CONSEGUI PENSAR E FAZER
+def verificador_de_senha():
+  try:
+    senha = input('Senha: [Min 8 caracters + 1 numero]: ')
+    split_senha = list(senha)
+    numbers = '0, 1, 2, 3, 4, 5, 6, 7, 8, 9'
+    flag = False
+
+    if len(senha) < 8:
+      print('Senha deve possuir no mínimo 8 caracteres!')
+    else:
+      for i in split_senha:
+        for j in numbers:
+          if i == j:
+            flag = True
+    if flag == True:
+      print('Senha aceita!')
+    else:
+      print('A senha deve possuir pelo menos um número!')
+  except Exception as e:
+    print('Erro: ', e)
+  else: 
+    print('Programa executado.')
+  finally:
+    print('Programa encerrado.')
+
+verificador_de_senha()
